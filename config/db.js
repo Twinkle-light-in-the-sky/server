d6967b336b8ed24b6b6fac774c5a194376575d81
Twinkle-light-in-the-sky <Barsiktv1324@gmail.com>
2025-03-31 23:01:12 +0500

    update db

--- config/db.js
+++ config/db.js
@@ -1,23 +1,26 @@
 // server/config/db.js
 const mysql = require('mysql');
 
-// Создаем подключение к базе данных
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST || '127.0.0.1', // Обычно localhost для OpenServer
-    user: process.env.DB_USER || 'root',      // Имя пользователя (по умолчанию root)
-    password: process.env.DB_PASSWORD || '',   // Пароль (по умолчанию пустой)
-    database: process.env.DB_NAME || 'Startset', // Имя вашей базы данных
+// Создаем пул подключений вместо одного подключения
+const pool = mysql.createPool({
+    host: 'barsikec.beget.tech',
+    user: 'barsikec_er',
+    password: 'Wertikal229', // Замените на ваш пароль
+    database: 'barsikec_er',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-// Подключаемся к базе данных
-connection.connect((err) => {
+// Проверяем подключение
+pool.getConnection((err, connection) => {
     if (err) {
         console.error(`Ошибка подключения к базе данных: ${err.stack}`);
         return;
     }
     console.log('Успешно подключено к базе данных MySQL.');
+    connection.release();
 });
 
-// Экспортируем подключение для использования в других файлах
-module.exports = connection;
+module.exports = pool;
 
