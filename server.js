b322f45caa5a06beb882f4d278185afa81d00895
Twinkle-light-in-the-sky <Barsiktv1324@gmail.com>
2025-04-07 11:28:22 +0500

    update

--- server.js
+++ server.js
@@ -1980,28 +1980,27 @@ db.query(checkForeignKeyQuery, (err, results) => {
                 console.error('Ошибка при удалении внешнего ключа:', err);
             } else {
                 console.log('Внешний ключ успешно удален');
-                
-                // Создаем новую связь с таблицей templates
-                const addForeignKeyQuery = `
-                    ALTER TABLE orders 
-                    ADD CONSTRAINT orders_template_id_fk 
-                    FOREIGN KEY (template_id) 
-                    REFERENCES templates(id)
-                `;
-                
-                db.query(addForeignKeyQuery, (err) => {
-                    if (err) {
-                        console.error('Ошибка при создании нового внешнего ключа:', err);
-                    } else {
-                        console.log('Новый внешний ключ успешно создан');
-                    }
-                });
             }
         });
     } else {
         console.log('Внешний ключ orders_ibfk_2 не существует, пропускаем удаление');
-        
-        // Создаем новую связь с таблицей templates
+    }
+});
+
+// Проверяем существование нового внешнего ключа
+const checkNewForeignKeyQuery = `
+    SELECT CONSTRAINT_NAME 
+    FROM information_schema.TABLE_CONSTRAINTS 
+    WHERE CONSTRAINT_SCHEMA = DATABASE() 
+    AND TABLE_NAME = 'orders' 
+    AND CONSTRAINT_NAME = 'orders_template_id_fk'
+`;
+
+db.query(checkNewForeignKeyQuery, (err, results) => {
+    if (err) {
+        console.error('Ошибка при проверке нового внешнего ключа:', err);
+    } else if (results.length === 0) {
+        // Если ключ не существует, создаем его
         const addForeignKeyQuery = `
             ALTER TABLE orders 
             ADD CONSTRAINT orders_template_id_fk 
@@ -2016,6 +2015,8 @@ db.query(checkForeignKeyQuery, (err, results) => {
                 console.log('Новый внешний ключ успешно создан');
             }
         });
+    } else {
+        console.log('Внешний ключ orders_template_id_fk уже существует, пропускаем создание');
     }
 });
 
